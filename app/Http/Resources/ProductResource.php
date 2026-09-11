cd8371235b25485700e0e1028eb3d6e8cbbd4bcf
anggarchmdi <anggarchmdi@gmail.com>
2026-09-11 10:20:54 +0700

    feat: crete resource product

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'category_id' => $this->category_id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'price' => (string) $this->price,
+            'minimum_order' => (int) $this->minimum_order,
+            'addons_enabled' => (bool) $this->addons_enabled,
+            'image' => $this->image,
+            'is_active' => (bool) $this->is_active,
+            'category' => $this->whenLoaded('category', function () {
+                return [
+                    'id' => $this->category->id,
+                    'name' => $this->category->name,
+                    'slug' => $this->category->slug,
+                ];
+            }),
+            'addon_groups' => AddonGroupResource::collection($this->whenLoaded('addonGroups')),
+        ];
+    }
+}
